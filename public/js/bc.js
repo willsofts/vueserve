0318938a305fea66aec14b2469c6664f3c5a72f6
tassun <32536248+tassun@users.noreply.github.com>
2024-09-11 10:06:49 +0700

    update sources

--- public/js/bc.js
+++ public/js/bc.js
@@ -5,16 +5,10 @@ function startReceiveBroadcast() {
         socket.on('broadcast-message', function(msg) {
             console.log("broadcast-message:",msg);
             let div = $("<div class='bc-layer'></div>");
-            let link = $("<a href=\"javascript:void(0)\" class=\"bc-close\" aria-label=\"close\"></a>").html("<em class='fa fa-close'></em>");
+            let link = $("<a href=\"javascript:void(0)\" class=\"bc-close\" aria-label=\"close\"></a>").html("&times;");
             link.click(function() { div.remove(); });
             let span = $("<span></span>").html(msg.message);
-            let body = $("body");
-            let isz = body.find("div.bc-layer").length;
-            if(isz > 0 && isz < 10) {
-                let bottom = isz * 50;
-                div.css({bottom: bottom});
-            }
-            div.append(link).append(span).appendTo(body);
+            div.append(link).append(span).appendTo($("body"));
         });
     }
 }
