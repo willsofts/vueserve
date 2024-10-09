42ef38472e326795e30af69085de2b068848c24e
tassun <32536248+tassun@users.noreply.github.com>
2024-10-09 14:31:28 +0700

    Update docker_push.bat

--- docker_push.bat
+++ docker_push.bat
@@ -1,9 +1,10 @@
 set REGISTRY_USER=willsofts
 set REGISTRY_PASSWORD=Tassan@2024
+set IMAGE_NAME=willsofts/vueserve
 set APP_NAME=willsofts/vueserve
 set APP_VERSION=1.0.0
 docker login --username %REGISTRY_USER% --password %REGISTRY_PASSWORD%
-docker tag willsofts/vueserve "%APP_NAME%":%APP_VERSION%
-docker tag willsofts/vueserve "%APP_NAME%":latest
+docker tag %IMAGE_NAME% "%APP_NAME%":%APP_VERSION%
+docker tag %IMAGE_NAME% "%APP_NAME%":latest
 docker push "%APP_NAME%":%APP_VERSION%
 docker push "%APP_NAME%":latest
